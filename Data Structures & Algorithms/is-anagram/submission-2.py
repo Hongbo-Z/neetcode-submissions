906443124474afec4098e3ec68cee17937079767
Hongbo-Z <90640554+Hongbo-Z@users.noreply.github.com>
2026-06-16 14:05:51 +0100

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        counter_s = collections.defaultdict(int)
+        counter_t = collections.defaultdict(int)
+
+        for i in range(len(s)):
+            counter_s[s[i]] += 1
+            counter_t[t[i]] += 1
+        return counter_s == counter_t
