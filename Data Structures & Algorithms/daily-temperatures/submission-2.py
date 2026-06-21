67d66764edb7343ceb6727fd6855daa1af0ddc46
Hongbo-Z <90640554+Hongbo-Z@users.noreply.github.com>
2026-06-21 20:42:01 +0100

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # Brute force
+        res = []
+        for i in range(len(temperatures)):
+            for j in range(i, len(temperatures)):
+                if temperatures[j] > temperatures[i]:
+                    res.append(j-i)
+                    break     
+                if j == len(temperatures) - 1:
+                    res.append(0)   
+        return res
