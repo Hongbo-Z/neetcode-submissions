2dadc0a8e3b9dff58a6edd60e5fd54f1080bfe38
Hongbo-Z <90640554+Hongbo-Z@users.noreply.github.com>
2026-06-29 19:43:52 +0100

    Add: generate-parentheses - submission-4

---
+++ Data Structures & Algorithms/generate-parentheses/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        stack = []
+
+        def dfs(leftPar, rightPar):
+            if leftPar == rightPar == n:
+                res.append("".join(stack))
+            if leftPar < n:
+                stack.append('(')
+                dfs(leftPar +1, rightPar)
+                stack.pop()
+            if rightPar < leftPar:
+                stack.append(')')
+                dfs(leftPar, rightPar + 1)
+                stack.pop()
+        dfs(0,0)
+        return res
