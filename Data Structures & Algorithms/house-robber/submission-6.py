aa98e55b7458d4d0dc43285585b76e20e6ac29fd
Hongbo-Z <90640554+Hongbo-Z@users.noreply.github.com>
2026-08-14 11:09:03 +0100

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # dp[i] represent the maximum total money we can rob at position i
+
+        if len(nums) == 1:
+            return nums[0]
+        n = len(nums)
+        dp = [0]*n
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+
+        for i in range(2, n):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        return dp[n-1]
