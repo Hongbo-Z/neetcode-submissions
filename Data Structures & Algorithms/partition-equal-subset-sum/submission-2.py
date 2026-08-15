c47b3c6ca5b08f13eeed9d498d1267fb0f240b31
Hongbo-Z <90640554+Hongbo-Z@users.noreply.github.com>
2026-08-15 12:34:06 +0100

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+            
+        dp = set() # record all possible subset summation we can make using the numbers we have processed so far.
+        dp.add(0) # if we choose nothing, the sum is 0.
+        # the above two lines can be replace with: dp = {0}
+
+        target = total // 2 # using the floor division to get the integer
+
+        for num in nums:
+            next_dp = dp.copy()
+            for item in dp:
+                next_dp.add(item + num)
+            dp = next_dp
+        return target in dp
+            
