962638f8c5db96f1a513203495aaffa899cc5196
Hongbo-Z <90640554+Hongbo-Z@users.noreply.github.com>
2026-08-18 12:19:16 +0100

    Add: missing-number - submission-5

---
+++ Data Structures & Algorithms/missing-number/submission-5.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        sum1, sum2 = 0, 0
+        for i in range(len(nums)+1):
+            sum1 += i
+        sum2 = sum(nums)
+
+        return sum1 - sum2
