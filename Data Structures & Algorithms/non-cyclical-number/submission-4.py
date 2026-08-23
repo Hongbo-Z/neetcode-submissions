7b7717a1c61d2d2fc044e57a7e76559455cd37ac
Hongbo-Z <90640554+Hongbo-Z@users.noreply.github.com>
2026-08-23 09:49:45 +0100

    Add: non-cyclical-number - submission-4

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visit = set()
+        while n not in visit:
+            visit.add(n)
+            n = self.sumOfSquare(n)
+            if n == 1:
+                return True
+        return False
+
+
+
+    def sumOfSquare(self, n):
+        sum = 0
+        while n:
+            sum += (n % 10)**2
+            n = n //10
+        return sum
+
+
